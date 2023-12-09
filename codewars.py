d025411e86fe6c42ab3f23605e4461fc1c5ac1d4
n_k <pekolian-dengi@yandex.ru>
2023-12-09 22:53:03 +0300

    test_commit

--- codewars.py
+++ codewars.py
@@ -1,3 +1,4 @@
+#вариант  1
 def remainder(a,b):
     if min(a, b) == 0:
         return None
@@ -5,8 +6,21 @@ def remainder(a,b):
         return a % b
     else:
         return b % a
-
-
+#вариант 2
+def remainder1(a,b):
+    if min(a,b)!=0: return max(a,b)%min(a,b)
+#вариант 3
+def remainder3(a,b):
+    return max(a, b) % min(a, b) if min(a, b) else None
+#вариант 4
+def remainder4(a:int, b:int)->int:
+    '''Returns remainder of the larger of two numbers
+    being divided by the smaller of two numbers.
+    Returns None for divide by zero.'''
+    try:
+        return max(a, b) % min(a, b)
+    except ZeroDivisionError:
+        return None
 
 
 
