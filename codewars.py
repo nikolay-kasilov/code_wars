012215221e90023c77e0d8288f03db5bfc8bbcd2
n_k <pekolian-dengi@yandex.ru>
2023-12-09 22:47:48 +0300

    new_commit

--- codewars.py
+++ codewars.py
@@ -1,11 +1,17 @@
 def remainder(a,b):
-      a =int(17)
-      b = int(5)
-      c = a % b
-      if a % b >0:
-            print(c)
-      else:
-            return()
+    if min(a, b) == 0:
+        return None
+    elif a > b:
+        return a % b
+    else:
+        return b % a
+
+
+
+
+
+
+
 
 
 
