a8febef5ec76723f03880811d4ca4dcfce3a9a6b
n_k <pekolian-dengi@yandex.ru>
2023-12-14 12:26:37 +0300

    commit_code

--- codewars.py
+++ codewars.py
@@ -22,12 +22,48 @@
 #     except ZeroDivisionError:
 #         return None
 
-def even_or_odd(number):
-    pass
-    if number % 2 == 0:
-      return "Even"
+# def even_or_odd(number):
+#     pass
+#     if number % 2 == 0:
+#       return "Even"
+#     else:
+#       return "Odd"
+
+flower1 = 5
+flower2 = 8
+def lovefunc(flower1, flower2):
+    while \
+    flower1 % 2 ==0 and flower2 % 2 != 0:
+        return True
     else:
-      return "Odd"
+        return False
+
+"""
+Сделайте функцию, которая создаст новый список из этого, заменив None на 0
+"""
+
+test_lst = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [None, 8, 9],
+    [10, 11, 12],
+    [13, None, 15],
+    [16, 17, 18],
+    [19, 20, 21],
+    [22, 23, None],
+]
+for i in range(len(test_lst)):
+    for j in range(len(test_lst[i])):
+        if test_lst[i][j] == None:
+            test_lst[i][j] = 0
+
+
+
+print (test_lst)
+
+
+
+
 
 
 
